aac3fb222e23ba6c1deb19bffe40980477354a42
gandoo <m_altyeib@yahoo.com>
2020-04-17 23:21:21 +0200

    add login layout and fix some import issues

---
+++ src/actions/shared.js
@@ -0,0 +1,12 @@
+import { getInitialData } from "../utils/dataAPI";
+import { getUsers } from "../actions/users";
+import { getQuestions } from "../actions/questions";
+
+export function handleInitialData() {
+  return dispatch => {
+    return getInitialData().then(({ users, questions }) => {
+      dispatch(getUsers(users));
+      dispatch(getQuestions(questions));
+    });
+  };
+}
