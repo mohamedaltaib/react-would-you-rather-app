4780a3fb8b3bcd555a2f08b2a15024cf98112e72
gandoo <m_altyeib@yahoo.com>
2020-04-18 20:38:05 +0200

    add question card and improve layout ux

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import { connect } from "react-redux";
 
 import Nav from "./Nav";
+import Question from "./Question";
+import { NavLink } from "react-router-dom";
 
 class Home extends React.Component {
   state = {
-    activeTab: "unanswered"
+    activeTab: "unasnswered"
   };
 
   componentDidMount() {
@@ -17,7 +19,6 @@ class Home extends React.Component {
 
   switchActivTab = (e, name) => {
     e.preventDefault();
-    console.log(this.state.activeTab);
     this.setState({
       activeTab: name
     });
@@ -29,13 +30,32 @@ class Home extends React.Component {
     const unansweredActive = activeTab === "unasnswered" ? "active" : "";
     const answeredActive = activeTab === "unasnswered" ? "" : "active";
 
+    const { questions, users, authedUser } = this.props;
+
+    const userAnswers = authedUser
+      ? Object.keys(users[authedUser].answers)
+      : [];
+    const unanserrdQuestions = questions
+      .filter(question => !userAnswers.includes(question.id))
+      .sort((a, b) => b.timestamp - a.timestamp);
+    const anserrdQuestions = questions
+      .filter(question => userAnswers.includes(question.id))
+      .sort((a, b) => b.timestamp - a.timestamp);
+
     return (
       <div>
         <Nav />
 
         <br />
 
-        <div className="ui two top attached buttons">
+        <div
+          className="ui two top attached buttons"
+          style={{
+            width: "80vw!important",
+            marginLeft: "10vw",
+            marginRight: "10vw"
+          }}
+        >
           <div
             className={"ui button " + unansweredActive}
             onClick={e => this.switchActivTab(e, "unasnswered")}
@@ -49,15 +69,43 @@ class Home extends React.Component {
             Ansnswered
           </div>
         </div>
-        {activeTab == "unasnswered" ? (
-          <div className="ui attached segment">
-            <p>un ansered questions </p>
-          </div>
-        ) : (
-          <div className="ui attached segment">
-            <p>ansered questions </p>
-          </div>
-        )}
+        <div
+          style={{
+            width: "80vw!important",
+            marginLeft: "10vw",
+            marginRight: "10vw"
+          }}
+        >
+          {activeTab == "unasnswered" ? (
+            <div className="ui attached segment">
+              {questions &&
+                unanserrdQuestions.map(question => {
+                  return (
+                    <Question
+                      key={question.id}
+                      question={question}
+                      auther={users[authedUser].name}
+                      answered={false}
+                    />
+                  );
+                })}
+            </div>
+          ) : (
+            <div className="ui attached segment">
+              {questions &&
+                anserrdQuestions.map(question => {
+                  return (
+                    <Question
+                      key={question.id}
+                      question={question}
+                      auther={users[authedUser].name}
+                      answered={true}
+                    />
+                  );
+                })}
+            </div>
+          )}
+        </div>
 
         <br></br>
       </div>
@@ -65,6 +113,10 @@ class Home extends React.Component {
   }
 }
 
-export default connect(({ authedUser }) => {
-  return { authedUser };
+export default connect(({ authedUser, questions, users }) => {
+  return {
+    authedUser,
+    users: questions !== null ? users : {},
+    questions: questions !== null ? Object.values(questions) : []
+  };
 })(Home);
